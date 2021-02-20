31f2ef70a74370fc3a206b653528b468c21152bb
SAIDAKTAOU <s.aktaou@gmail.com>
2021-02-20 23:35:20 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule} from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {AnimalsService} from './animals-service';
+import {FilterDropdownComponent} from './filter-dropdown/filter-dropdown.component';
+import {CardComponent} from './card/card.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilterDropdownComponent,
+    CardComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
+  ],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap:[AppComponent]
+})
+export class AppModule {
+}
